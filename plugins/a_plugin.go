f5858bc7b25c077693d378e6aabfaa501a9867a3
millken <millken@gmail.com>
2015-04-28 18:17:30 +0800

    update zone config, A record

--- plugins/a_plugin.go
+++ plugins/a_plugin.go
@@ -9,10 +9,25 @@ import (
 	"github.com/miekg/dns"
 )
 
+/*
+ *
+ type & 1  view
+ type & 2  weight
+*/
+
+const (
+	VIEW   = 1
+	WEIGHT = 2
+)
+
+var upChooseRecord = -1
+var currentWeight int = 0
+
 type RecordAPlugin struct {
 	EdnsAddr   net.IP
 	RemoteAddr net.IP
 	RRheader   dns.RR_Header
+	Conf       map[string]interface{}
 }
 
 func (this *RecordAPlugin) New(edns, remote net.IP, rr_header dns.RR_Header) {
@@ -20,19 +35,69 @@ func (this *RecordAPlugin) New(edns, remote net.IP, rr_header dns.RR_Header) {
 	this.RemoteAddr = remote
 	this.RRheader = rr_header
 }
+
 func (this *RecordAPlugin) Filter(conf map[string]interface{}) (answer []dns.RR, err error) {
-	log.Printf("conf : %+v, %T", conf, conf["value"])
-	if _, ok := conf["value"]; !ok {
-		return
+	log.Printf("conf : %+v", conf)
+	this.Conf = conf
+	if _, ok := conf["type"]; !ok {
+		return this.NormalRecord(this.Conf["records"].([]interface{}))
 	}
-	for _, v := range conf["value"].([]interface{}) {
+	record_type := conf["type"].(uint64)
+	if record_type&VIEW == VIEW {
+		return this.ViewRecord()
+	}
+	if record_type&WEIGHT == WEIGHT {
+		return this.WeightRecord()
+	}
+	return
+}
+
+func (this *RecordAPlugin) NormalRecord(records []interface{}) (answer []dns.RR, err error) {
+	for _, v := range records {
 		ip := net.ParseIP(strings.TrimSpace(v.(string)))
 		if ip == nil {
 			continue
 		}
 		answer = append(answer, &dns.A{this.RRheader, ip})
 	}
-	return answer, nil
+	return
+}
+
+//http://www.wangshangyou.com/go/126.html
+func (this *RecordAPlugin) WeightRecord() (answer []dns.RR, err error) {
+	maxweight := this.getMaxWeight()
+	//log.Printf("maxweight : %d", maxweight)
+	records := this.Conf["records"].([]interface{})
+	for {
+		upChooseRecord = (upChooseRecord + 1) % len(records)
+		if upChooseRecord == 0 {
+			currentWeight = currentWeight - 2
+			if currentWeight <= 0 {
+				currentWeight = maxweight
+			}
+		}
+		if weight := int(records[upChooseRecord].(map[string]interface{})["weight"].(uint64)); weight >= currentWeight {
+			//log.Printf("%+v", records[upChooseRecord])
+			return this.NormalRecord(records[upChooseRecord].(map[string]interface{})["record"].([]interface{}))
+		}
+	}
+
+	return
+}
+
+func (this *RecordAPlugin) ViewRecord() (answer []dns.RR, err error) {
+	return
+}
+
+func (this *RecordAPlugin) getMaxWeight() int {
+	maxweight := 0
+	for _, v := range this.Conf["records"].([]interface{}) {
+		weight := int(v.(map[string]interface{})["weight"].(uint64))
+		if weight > maxweight {
+			maxweight = weight
+		}
+	}
+	return maxweight
 }
 
 func init() {
